0da93622d5c9c068be93970f8da2e254bd65d5ca
Isaiah <ijandicos90@gmail.com>
2022-06-08 22:04:21 -0400

    Complete Assignment

--- src/Clock.js
+++ src/Clock.js
@@ -7,6 +7,10 @@ function Clock() {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    return function cleanup(){
+      clearInterval(timerID)
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
